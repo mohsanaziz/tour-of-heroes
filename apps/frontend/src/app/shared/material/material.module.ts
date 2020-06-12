43bf387dd10b6f6d51684e93b19e630ad6c8f531
Mohsan <mohsan.aziz@gmail.com>
2020-06-12 20:09:06 +0200

    feat: add heroe search

--- apps/frontend/src/app/shared/material/material.module.ts
+++ apps/frontend/src/app/shared/material/material.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  exports: [MatToolbarModule, MatListModule],
+  exports: [MatToolbarModule, MatListModule, MatAutocompleteModule, MatFormFieldModule, MatInputModule],
 })
 export class MaterialModule {}
