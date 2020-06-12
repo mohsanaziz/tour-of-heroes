fcda55942accacf3d270bd014437f77133f372c2
Mohsan <mohsan.aziz@gmail.com>
2020-06-12 18:36:36 +0200

    feat: add router on navigation toolbar

--- apps/frontend/src/app/shared/shared.module.ts
+++ apps/frontend/src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { MaterialModule } from './material/material.module';
 
 @NgModule({
-  exports: [CommonModule, MaterialModule],
+  exports: [CommonModule, MaterialModule, RouterModule],
 })
 export class SharedModule {}
